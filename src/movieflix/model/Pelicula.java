2785254ca79343c5304f3a2bcac1f27d297cff31
Edmund Home <edmundhome@hotmail.com>
2019-09-30 17:05:59 +0200

    Clase Pelicula creada

---
+++ src/movieflix/model/Pelicula.java
@@ -0,0 +1,36 @@
+package movieflix.model;
+
+public class Pelicula {
+	
+	
+	
+	private int id, anyo, idCat;
+	private String nombre;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAnyo() {
+		return anyo;
+	}
+	public void setAnyo(int anyo) {
+		this.anyo = anyo;
+	}
+	public int getIdCat() {
+		return idCat;
+	}
+	public void setIdCat(int idCat) {
+		this.idCat = idCat;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
