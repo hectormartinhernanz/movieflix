09597db9cb9ec630be13f50e8ad9484a8511e4a2
Edmund Home <edmundhome@hotmail.com>
2019-09-30 23:19:35 +0200

    Añado clases modificar y mostrar

---
+++ src/movieflix/datos/Modificar.java
@@ -0,0 +1,17 @@
+package movieflix.datos;
+
+public class Modificar {
+
+	public Modificar() {
+		
+	}
+	
+	public void modificarUsuario() {
+		
+	}
+	
+	public void modificarPelicula() {
+		
+	}
+	
+}
---
+++ src/movieflix/datos/Mostrar.java
@@ -0,0 +1,20 @@
+package movieflix.datos;
+
+public class Mostrar {
+	
+
+	public void mostrarUsuario() {
+		
+	}
+	
+	public void mostrarPelicula() {
+		
+	}
+	
+	public void mostrarListadoPeliculas() {
+		
+	}
+	
+
+
+}
