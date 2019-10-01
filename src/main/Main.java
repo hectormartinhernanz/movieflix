8921b72b876d8c6d97dc541bc0d21e5de4ae5f87
Edmund Home <edmundhome@hotmail.com>
2019-10-01 11:32:16 +0200

    Revert "Estoy probando"
    This reverts commit bab30dcdf705f102f9610d80a4ab941a4e0ea9ae.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,11 @@
 package main;
 
-import movieflix.servicios.Modificar;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 Modificar.mostrarListadoPeliculas();
+		 movieflix.servicios.Modificar.mostrarListadoPeliculas();
 	}
 
 }
