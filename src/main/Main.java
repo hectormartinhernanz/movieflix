cc4ab15caf4b8a1e33a41e735cd817b77b3e0003
hectormartinhernanz <nevercreature@gmail.com>
2019-10-01 11:03:47 +0200

    Metodo Pruebas de clase Test

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,8 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 movieflix.servicios.Modificar.mostrarListadoPeliculas();
+
+		 Test.pruebas();
 	}
 
 }
