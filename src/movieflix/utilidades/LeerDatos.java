0bdf93be60b8b255ed1a1e133f3e0b74e4d3258b
Edmund Home <edmundhome@hotmail.com>
2019-10-01 09:51:25 +0200

    Creados los métodos LeerDatos
    Creadosl los métodos para introducir datos por teclado

--- src/movieflix/utilidades/LeerDatos.java
+++ src/movieflix/utilidades/LeerDatos.java
@@ -1,6 +1,21 @@
 package movieflix.utilidades;
+import java.util.Scanner;
 
 public class LeerDatos {
 
-	public LeerDatos() {}
+	public LeerDatos() {
+		
+	}
+	
+	public static int tecladoInt(){		
+		return new Scanner(System.in).nextInt();
+	}
+	
+	public static String tecladoString(){		
+		return new Scanner(System.in).next();
+	}	
+	
+	public static boolean tecladoBoolean(){		
+		return new Scanner(System.in).nextBoolean();
+	}
 }
