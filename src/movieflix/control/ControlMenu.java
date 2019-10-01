4e0632c78be747e9fc4a76de69611c5ff7b48d0f
rocioBorrazas <rocio.borrazas@gmail.com>
2019-10-01 12:47:43 +0200

    creandometodo procesarOpcion

--- src/movieflix/control/ControlMenu.java
+++ src/movieflix/control/ControlMenu.java
@@ -1,5 +1,18 @@
 package movieflix.control;
 
 public class ControlMenu {
+	
+	//private IMovieServ = new MovieServ ();
+	
+	public void procesarOpcion () {
+		boolean continuar = true
+		
+	
+			
+		}
+	}
+	
+	
+	
 
 }
