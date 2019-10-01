1f769fa61c447ab23646edb382b7ecfedb3859b8
rocioBorrazas <rocio.borrazas@gmail.com>
2019-10-01 16:49:46 +0200

    metodo seleccionarOpciones
    comprobado falta cargar datos de MovieServ

--- src/movieflix/control/ControlMenu.java
+++ src/movieflix/control/ControlMenu.java
@@ -1,9 +1,10 @@
 package movieflix.control;
 
+import movieflix.servicios.MovieServ;
 import movieflix.utilidades.LeerDatos;
-import utilidades.Datos;
 
-/*
+
+/**
  * *Esta clase muestra las opciones al usuario del programa y gestiona las decisiones del mismo
  * @author 
  */
@@ -20,96 +21,99 @@ public class ControlMenu {
 		System.out.println("");
 		System.out.println("ELIJA UNA OPCIÓN");
 		System.out.println("");
-		System.out.println("(01) Incluir película en listado general");
-		System.out.println("(02) Dar de alta nuevo usuario");
-		System.out.println("(03) Dar de baja usuario existente");
-		System.out.println("(04) Modificar datos de usuario existente");
-		System.out.println("(05) Mostrar datos de usuario existente");
-		System.out.println("(06) Mostrar películas suscritas por un usuario");
-		System.out.println("(07) Mostrar listado general de películas");
-		System.out.println("(08) ??? ");
+		System.out.println("(1) Incluir película en listado general");
+		System.out.println("(2) Dar de alta nuevo usuario");
+		System.out.println("(3) Dar de baja usuario existente");
+		System.out.println("(4) Modificar datos de usuario existente");
+		System.out.println("(5) Mostrar datos de usuario existente");
+		System.out.println("(6) Mostrar películas suscritas por un usuario");
+		System.out.println("(7) Mostrar listado general de películas");
+		System.out.println("(8) ??? ");
 		System.out.println("(0) SALIR");
 		System.out.println("__________________________________________________________________");
-		// procesarOpcion();
-
+		 
+		seleccionOpciones();
+return true;
+	
 	}
 	
+/**
+ *  @author Rocio Borrazas
+*/
+	//METODO PARA LEER EL MENU Y ELEGIR OPCIONES
 	
+	//public static void procesarOpcion() {
+	//	boolean seguir = true;
+		//LeerDatos.tecladoInt()
+	//	do {
+	//		seguir = mostrarMenu();
+	//	} while (seguir);
+	//	System.out.println("---Fin de la Sesión---");
+	// }
 
-	public static void procesarOpcion() {
-		boolean seguir = true;
-		do {
-			mostrarMenu();
-			seguir = mostrarMenu();
-		} while (seguir);
-		System.out.println("---Fin de la Sesión---");
-	}
-
-public boolean seleccionOpciones () {
+public static boolean seleccionOpciones () {
 	
     boolean continuar = true;		
 	
 	try {
-		    switch(// LeerDatos.LeerInt) {
+		    switch(LeerDatos.tecladoInt()) {
 		    		
 		    		
 		    //INCLUIR PELICULA EN LISTADO GENERAL
 			case 1: 
-					MovieServ.;
+					System.out.println("MovieServ.;1"); 
 					break;
 		
-
 			//DAR DE ALTA NUEVO USUARIO	
 			case 2:
-					MovieServ.;
+					System.out.println("MovieServ.;2");
 					break;
-	
 	
 			//DA DE BAJA USUARIO EXISTENTE
 			case 3:
-					MovieServ. ;
+					System.out.println("MovieServ.;3");
 					break;
 	
 			//MODIFICAR DATOS DE USUARIO EXISTENTE 
 			case 4:
-					MovieServ. ;
+					System.out.println("MovieServ.;4");
 					break;
 	
 			//MOSTRAR DATOS DE USUARIO EXISTENTE	
 			case 5:
-				MovieServ. ;
+				System.out.println("MovieServ.;5");
 				break;	
 	
 	        //MOSTRAR PELICULAS CREADAS POR UN USUARIO
 			case 6:
-				MovieServ. ;
+				System.out.println("MovieServ.;6");
 				break;
 	
 			//MOSTRAR LISTADO GENERAL DE PELICULAS
 			case 7:
-				MovieServ. ;
+				System.out.println("MovieServ.;7");
 				break;
 	
 			//PARA HACER PRUEBAS
 			case 8:
-				MovieServ. ;
+				System.out.println("MovieServ.;8");
 				break;
 	
 
 			case 0:
-				continuar = salir();
+				System.out.println("Hasta Luego!");
+				System.exit(0);
 				break;
-			}
+			 }
 	
-	} 		catch (Exception e) {
-			System.out.println("error: " + e.toString());
-}
+	         }catch (Exception e) {
+			System.out.println("error: " + e.toString()  + "    Entrada de teclado errónea" );
+			mostrarMenu();
+			
+	         }
 	return continuar;
+	
 
 	}
 
-
-//private boolean salir() throws Exception {
-//String sino = Datos.recogeString("   ¿Está seguro?(S/N)");
-//return (sino.toUpperCase().charAt(0) != 'S');
 }
