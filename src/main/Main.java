bab30dcdf705f102f9610d80a4ab941a4e0ea9ae
Edmund Home <edmundhome@hotmail.com>
2019-10-01 11:29:34 +0200

    Estoy probando

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,12 @@
 package main;
 
+import movieflix.servicios.Modificar;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 movieflix.servicios.Modificar.mostrarListadoPeliculas();
+		 Modificar.mostrarListadoPeliculas();
 	}
 
 }
