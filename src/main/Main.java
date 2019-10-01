1d4684ba879f2e6958f434d6c2f893be0847fce3
Edmund Home <edmundhome@hotmail.com>
2019-10-01 18:17:31 +0200

    Carga datos

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import movieflix.control.ControlMenu;
 import movieflix.datos.Datos;
 public class Main {
 	public static void main(String[] args) {
+		new Datos().cargarInicial();
 		ControlMenu.mostrarMenu();
 	}
 
